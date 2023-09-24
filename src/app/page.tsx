c91ffb6b99e6ecfaafa746f01baf9d773d931256
Fran <fran.p.21@gmail.com>
2023-09-24 13:05:07 -0300

    Add logic to generate board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Board } from "@/components/Board";
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-center">
-      <Board height={6} width={6} />
+      <Board difficulty="easy" />
     </main>
   );
 }
